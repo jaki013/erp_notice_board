f3fcb551ed10ed1246d9a54af19fe71b43c34695
Jagadeesh killi <killijagadeesh2468@gmail.com>
2020-05-13 01:32:29 +0530

    modified to fetch all cdc notifications one by one

--- app.js
+++ app.js
@@ -2,15 +2,8 @@
  * app.js : front page of erp.
  */
 
-const http = require('http');
-const https = require('https');
 const Promise = require('promise');
 const Axios = require('axios');
-const SetCookie = require('set-cookie-parser');
-const QueryString = require('query-string');
-const ReadLineSync = require('readline-sync');
-
-const Login = require('./login');
 
 /**end points */
 const PENDING_JOBS_URL = 'https://erp.iitkgp.ac.in/IIT_ERP3/getPendingJobCount.htm';
@@ -19,208 +12,243 @@ const UNREAD_MESSAGE_COUNT_URL = 'https://erp.iitkgp.ac.in/IIT_ERP3/getUnreadMai
 const MESSAGES_URL = 'https://erp.iitkgp.ac.in/IIT_ERP3/getMessages.htm';
 const TRAINING_PLACEMENT_URL = "https://erp.iitkgp.ac.in/TrainingPlacementSSO/TPStudent.jsp";
 const NOTICE_BOARD_URL = "https://erp.iitkgp.ac.in/TrainingPlacementSSO/Notice.jsp";
-const ERP_MONITORING_URL = "https://erp.iitkgp.ac.in/TrainingPlacementSSO/ERPMonitoring.htm?action=fetchData&jqqueryid=54";
+const ERP_MONITORING_URL = "https://erp.iitkgp.ac.in/TrainingPlacementSSO/ERPMonitoring.htm";
 const SHOW_CONTENT_URL = "https://erp.iitkgp.ac.in/TrainingPlacementSSO/ShowContent.jsp";
 
-/**_cookies should be an array */
-function getCookieString(cookies, path = '/IIT_ERP3') {
+const USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0';
 
-	var cookie_str = '';
+const Login = require('./login');
+const CDC = require('./cdc');
+const erpSession = require('./session');
 
-	//extract path cookies.
-	var path_cookies = [];
-	for (var i = 0; i < cookies.length; i++) {
-		if (cookies[i]['path'] === '/' || cookies[i]['path'] === path) {
-			path_cookies.push(cookies[i]);
-		}
-	}
 
-	if (path_cookies.length < 1) return cookie_str;
 
-	//prepare cookie string.
-	for (var i = 0; i < path_cookies.length; i++) {
-		cookie_str += path_cookies[i]['name'] + '=' + path_cookies[i]['value'];
-		if (i != path_cookies.length - 1) {
-			cookie_str += ';';
-		}
-	}
+async function getCDCMesaages(year){
 
-	return cookie_str;
+    let id = 1;
+    while(true){
+        
+        await setTimeout(() => {}, 1000);
+        let message = await CDC.getMessageContent(year, id);
+        console.log(`YEAR : ${year} ID : ${id}`);
+        console.log(message);
+
+        id++;
+    }
 }
 
-/**1. a routine to get number of unread mails. */
-function getUnreadMailCount(erp_cookies){
 
-    return new Promise((resolve, reject) => {
-        Axios({
-            url : UNREAD_MESSAGE_COUNT_URL,
-            method : 'POST',
-            headers : {
-                'Cookie' : getCookieString(erp_cookies, path='/IIT_ERP3'),
-            },
-            proxy : false,
+Login.loginERP().then(() => {
+    
+    CDC.initCDC().then(() => {
 
-        }).then((res) => {
-            // log_req_res(res);
-            console.log('#Unread Mail Count : ', res.data);
-        }).catch((err) => {
-            console.error(err);
-        })
+        getCDCMesaages('2019-2020');
     })
-}
+})
 
-/**2. a routine to get modules */
-function getModules(erp_cookies){
 
-    return new Promise((resolve, reject) => {
-        Axios({
-            url : MODULES_URL, 
-            method : 'POST',
-            headers : {
-                'Cookie' : getCookieString(erp_cookies, path='/IIT_ERP3'),
-            },
-            proxy : false,
 
-        }).then((res) => {
-            console.log(res.data);
 
-        }).catch((err) => {
-            console.log(err);
-        })
 
-    })
-}
 
-/**3. routine to post on /TrainingPlacementSSO */
-function TPStudent(erp_cookies){
-    return new Promise((resolve, reject) => {
-        Axios({
-            url : TRAINING_PLACEMENT_URL,
-            method : 'post',
-            headers : {
-                'Cookie' : getCookieString(erp_cookies, path='/TrainingPlacementSSO')
-            },
-            proxy : false,
 
-        }).then((res) => {
-            resolve(res);
 
-        }).catch((err) => {
-            reject(err);
-        })
-    })
-}
 
-function NoticeBoard(erp_cookies){
-    return new Promise((resolve, reject) => {
-        Axios({
-            url : NOTICE_BOARD_URL,
-            method : 'get',
-            headers : {
-                'Cookie' : getCookieString(erp_cookies, path='/TrainingPlacementSSO')
-            },
-            proxy : false,
 
-        }).then((res) => {
-            resolve(res);
 
-        }).catch((err) => {
-            reject(err);
-        })
-    })
-}
 
-function ERPMonitoring(erp_cookies){
-    return new Promise((resolve, reject) => {
-        Axios({
-            url : ERP_MONITORING_URL,
-            method : 'get',
-            headers : {
-                'Cookie' : getCookieString(erp_cookies, path='/TrainingPlacementSSO')
-            },
-            proxy : false, 
 
-        }).then((res) => {
-            resolve(res);
 
-        }).catch((err) => {
-            reject(err);
-        })
-    })
-}
 
-function Show_Content(erp_cookies, year, id){
-    return new Promise((resolve, reject) => {
-        Axios({
-            // url : SHOW_CONTENT_URL +'?' + 'year=' + year.toString() + '&' + 'id=' + id.toString(),
-            url : SHOW_CONTENT_URL, 
-            method : 'get',
-            params : {
-                year : year,
-                id : id,
-            },
-            headers :{
-                'Cookie' : getCookieString(erp_cookies, path='/TrainingPlacementSSO'),
-            },
-            proxy : false,
 
-        }).then((res) => {
-            console.log(res);
-            resolve(res);
 
-        }).catch((err) => {
-            reject(err);
-        })
-    })
-}
-/**log request, response */
-function log_req_res(res) {
 
-	console.log('\n\n');
-	console.log(res.request._header);
-	console.log(res.status, res.statusText);
-	console.log(res.headers);
-	console.log('\n\n');
-}
 
-Login.login_erp()
-    .then((erp_cookies) => {
 
-        // console.log(erp_cookies);
-        // getModules(erp_cookies);
-        TPStudent(erp_cookies)
-            .then((res) => {
-                // var _jsessCookie_sso = SetCookie.parse(res);
-                var tps_cookies = SetCookie.parse(res);
-                // console.log(tps_cookies);
-                erp_cookies = erp_cookies.concat(tps_cookies);
+// function getCDCMessages(){
+    
+//     /**STEP 1 getModules */
+//     return new Promise((resolve, reject) => {
+//         Axios({
+//             url : MODULES_URL, 
+//             method : 'POST',
+//             headers : {
+//                 'Cookie' : erpSession.getCookieString(path='/IIT_ERP3'),
+//                 'User-Agent' : USER_AGENT,
+//             },
+//             proxy : false,
+
+//         }).then((res) => {
+//             let modulesData = res.data;
+//             erpSession.saveModules(modulesData);
+//             resolve();
+
+//         }).catch((err) => {
+//             console.error('error in getModules');
+//             reject(err);
+//         })
+
+//     }).then(() => {
+
+//         /**STEP 2 : TPStudent.jsp */
+//         return new Promise((resolve, reject) => {
+//             Axios({
+//                 url : TRAINING_PLACEMENT_URL,
+//                 method : 'post',
+//                 data : QueryString.stringify({
+//                     ssoToken : erpSession.getCookieSSOToken(),
+//                     module_id : '26',
+//                     menu_id : '11',
+//                 }),
+//                 headers : {
+//                     'Cookie' : erpSession.getCookieString(path='/TrainingPlacementSSO'),
+//                     'User-Agent' : USER_AGENT,
+//                 },
+//                 proxy : false,
+    
+//             }).then((res) => {
+
+//                 let _tps_cookies = SetCookie.parse(res);
+//                 erpSession.saveCookies(_tps_cookies);
+//                 // console.log(res.data);
+//                 resolve();
+    
+//             }).catch((err) => {
+//                 console.error('error in getting TPStudent.jsp');
+//                 reject(err);
+//             })
+
+//         })
+
+//         /** STEP3 : get ERPMonitoring  */
+//     }).then(() => {
+//         // 
+//         console.log(erpSession.cookies);
+//         return new Promise((resolve, reject) => {
+//             Axios({
+//                 url : ERP_MONITORING_URL,
+//                 method : 'GET',
+//                 params : {
+//                     action : 'fetchData',
+//                     jqqueryid : '54',
+//                     _search : 'false',
+//                     nd : '158923827858',
+//                     rows : '20',
+//                     page : '1',
+//                     sidx : '',
+//                     sord : 'asc',
+//                     totalrows : '50',
+//                 },
+//                 headers : {
+//                     'Cookie' : erpSession.getCookieString(path='/TrainingPlacementSSO'),
+//                     'User-Agent' : USER_AGENT,
+//                 },
+//                 proxy : false, 
+    
+//             }).then((res) => {
+//                 console.log(res.config);
+//                 console.log(res.data);
+//                 resolve(res);
+
+//             }).catch((err) => {
+//                 console.log('error in get ERPMonitoring');
+//                 reject(err);
+//             })
+
+//         })
+
+//     })
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Login.loginERP()
+//     .then(() => {
+//         /**STEP 1  getModules */
+//         return new Promise((resolve, reject) => {
+//             Axios({
+//                 url : MODULES_URL, 
+//                 method : 'POST',
+//                 headers : {
+//                     'Cookie' : erpSession.getCookieString(path='/IIT_ERP3'),
+//                     'User-Agent' : USER_AGENT,
+//                 },
+//                 proxy : false,
+    
+//             }).then((res) => {
+//                 resolve(res.data); //response is a JSON object
+    
+//             }).catch((err) => {
+//                 console.error('error in getModules');
+//                 reject(err);
+//             })
+//         })
+
+//     })
+// Login.login_erp()
+//     .then((erp_cookies) => {
+
+//         // console.log(erp_cookies);
+//         // getModules(erp_cookies);
+//         TPStudent(erp_cookies)
+//             .then((res) => {
+//                 // var _jsessCookie_sso = SetCookie.parse(res);
+//                 var tps_cookies = SetCookie.parse(res);
+//                 // console.log(tps_cookies);
+//                 erp_cookies = erp_cookies.concat(tps_cookies);
                 
                 
-                // NoticeBoard(erp_cookies)
-                //     .then((res)=> {
-                //         console.log(res.data);
+//                 // NoticeBoard(erp_cookies)
+//                 //     .then((res)=> {
+//                 //         console.log(res.data);
 
 
-                //     })
+//                 //     })
 
-                // ERPMonitoring(erp_cookies)
-                //     .then((res) => {
-                //         console.log(res.data);
-                //     })
+//                 ERPMonitoring(erp_cookies)
+//                     .then((res) => {
+//                         console.log(res.data);
+//                     })
                 
-                Show_Content(erp_cookies, '2019-2020', 2065)
-                    .then((res) => {
+//                 // Show_Content(erp_cookies, '2019-2020', 2065)
+//                 //     .then((res) => {
                         
-                        console.log(res.data);
+//                 //         console.log(res.data);
 
-                    }).catch((err) => {
-                        console.log(err);
-                    })
+//                 //     }).catch((err) => {
+//                 //         console.log(err);
+//                 //     })
 
             
-            })
+//             })
         
-    })
-    .catch((err) => {
-        console.error(err);
-    })
+//     })
+//     .catch((err) => {
+//         console.error(err);
+//     })
